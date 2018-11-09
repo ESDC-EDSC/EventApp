798c6ecacf73a50b683f677b979c381db15f50b2
Glenn Mercier <gmercier1986@hotmail.com>
2018-11-09 14:40:28 -0500

    staged bilingual and added the french translations to Json

---
+++ src/app/featureFlags.ts
@@ -0,0 +1,10 @@
+export class FeatureFlags {
+    readonly search: boolean = true;
+    readonly bilingualBug: boolean = false;
+    readonly themeBug: boolean = true;
+    readonly map: boolean = true;
+    readonly ticketPurchase: boolean = true;
+    readonly sortEvents: boolean = true ;
+    readonly audioClip: boolean = true;
+    readonly calendar: boolean = true;
+  }
