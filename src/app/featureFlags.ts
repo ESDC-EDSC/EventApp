31127784a22af137e90452f00c7f97ec1fa36e29
Adam Andrews <35509135+andr0272@users.noreply.github.com>
2018-11-13 08:41:58 -0500

    Theme toggled to "true" for dry run demo (#43)

--- src/app/featureFlags.ts
+++ src/app/featureFlags.ts
@@ -1,7 +1,7 @@
 export class FeatureFlags {
     readonly search: boolean = true;
     readonly bilingualBug: boolean = true;
-    readonly theme: boolean = false;
+    readonly theme: boolean = true;
     readonly map: boolean = true;
     readonly ticketPurchase: boolean = true;
     readonly sortEvents: boolean = true ;
