b03392990457c4b72375c89fe9962d833ef5b3dc
Glenn Mercier <gmercier1986@hotmail.com>
2018-11-14 10:43:01 -0500

    Updateing features

--- src/app/featureFlags.ts
+++ src/app/featureFlags.ts
@@ -1,10 +1,10 @@
 export class FeatureFlags {
-    readonly search: boolean = false;
+    readonly search: boolean = true;
     readonly bilingualBug: boolean = false;
     readonly theme: boolean = false;
     readonly map: boolean = false;
-    readonly ticketPurchase: boolean = false;
+    readonly ticketPurchase: boolean = true;
     readonly sortEvents: boolean = false;
-    readonly audioClip: boolean = false;
-    readonly calendar: boolean = false;
+    readonly audioClip: boolean = true;
+    readonly calendar: boolean = true;
   }
